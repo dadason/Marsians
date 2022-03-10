79229bd12e73eba06c88249423e07aeb7a004e89
mamamochka <guidingproject@gmail.com>
2022-03-10 08:41:07 +0300

    я не совсем поняла как передать в адоесной строке адрес гланой странице, если его нет в главной странице, а есть только '/'

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for, request
 import json
-
 app = Flask(__name__)
 
+@app.route('/<page_title>')
+@app.route('/index/<page_title>')
+def index(page_title):
+    return render_template('base.html', title=page_title)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    #param = {}
-    #param['title'] = 'Заготовка'
-    return render_template('base.html',title='template')
+
+#
+# @app.route('/greeting/<username>')
+# def greeting(username):
+#     return render_template('base1.html', user=username, Title = username)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
