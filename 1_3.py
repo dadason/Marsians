a57d789cadcfd8bea9fe6b1fea1fc6a82c37a911
mamamochka <guidingproject@gmail.com>
2022-03-13 08:14:16 +0300

    для третьей задачи запускать 1_3.py

--- 1_3.py
+++ 1_3.py
@@ -1,16 +1,7 @@
-from flask import Flask, render_template,url_for
-import json
-app = Flask(__name__)
-#
-# @app.route('/<page_title>')
-@app.route('/index/<page_title>')
-def index(page_title):
-    return render_template('base.html', title=page_title)
+from flask import Flask, render_template
 
-# @app.route('/<profession>')
-@app.route('/trainings/<profession>')
-def trainings(profession):
-    return render_template('trainings.html', title=profession)
+app = Flask(__name__)
+
 
 @app.route('/list_prof/<lst>')
 def list_prof(lst):
@@ -20,10 +11,6 @@ def list_prof(lst):
                    'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
                    'штурман', 'пилот дронов']
     return render_template('list_prof.html', list=lst, professions=professions)
-
-
-
-
 
 
 if __name__ == '__main__':
